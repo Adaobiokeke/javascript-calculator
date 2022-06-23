2fc0bd760a910af27ce2a3967f637564eb7fdd3e
Adaobiokeke <adaobiotiji@yahoo.com>
2022-06-23 10:45:02 -0400

    javascript calculator

---
+++ scripts.js
@@ -0,0 +1,11 @@
+var num1 = window.prompt("enter a number")
+var num2 = window.prompt("enter another number")
+
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+// document.write(num1 + num2);
+// document.write(num1 - num2);
+// document.write(num1 / num2);
+document.write(num1 * num2);
